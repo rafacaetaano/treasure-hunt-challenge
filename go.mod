ecdbd786c8d34086d0b5946bf5190f03144d5b08
Rafael Ferreira Caetano <rcaetano0709@gmail.com>
2025-09-03 17:34:12 -0300

    feat(models): add User entity

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rafacaetaano/treasure-hunt-challenge
+
+go 1.25.0
---
+++ internal/models/user.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name      string    `gorm:"not null" json:"name"`
+	Username  string    `gorm:"not null;unique" json:"username"`
+	Password  string    `gorm:"not null" json:"password"`
+	Email     string    `gorm:"not null;unique" json:"email"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	Role      string    `gorm:"size:20;not null" json:"role"`
+}
+
+// TableName define explicitamente o nome da tabela no banco
+func (User) TableName() string {
+	return "users"
+}
